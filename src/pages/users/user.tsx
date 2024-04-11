79eff742bf2cc68beb0b3592c4ead1c13db9acca
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-04-11 23:29:59 +0200

    shortened the component a bit

--- src/pages/users/user.tsx
+++ src/pages/users/user.tsx
@@ -3,8 +3,7 @@ import { webHistorySearchParameters } from "../../web-history.tsx";
 import { WebHistoryRouteElementProps } from "../../library/history.tsx";
 
 export default function UserPage({ user }: WebHistoryRouteElementProps<"/users/:user">) {
-  const searchParameters = webHistorySearchParameters();
-  const theme = createMemo(() => searchParameters.get("theme"));
+  const theme = createMemo(() => webHistorySearchParameters().get("theme"));
 
   return (
     <h1>User#{user} page with theme {theme()}</h1>
