6f8e5db078cc8753e59b4edf75e62e6d2f99949d
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-04-11 23:07:17 +0200

    using the route enumeration instead of hardcoding the routes

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,21 @@
+import { Route } from './route';
 import { webHistoryPush, WebHistoryView } from './web-history';
 
 export function App() {
   return (
     <>
       <ul>
-        <li onClick={() => webHistoryPush({ route: "/" })}>
+        <li onClick={() => webHistoryPush({ route: Route.Home })}>
           <button>
             Home
           </button>
         </li>
-        <li onClick={() => webHistoryPush({ route: "/about" })}>
+        <li onClick={() => webHistoryPush({ route: Route.About })}>
           <button>
             About
           </button>
         </li>
-        <li onClick={() => webHistoryPush({ route: "/users/:user", parameters: { user: "123" } })}>
+        <li onClick={() => webHistoryPush({ route: Route.User, parameters: { user: "123" } })}>
           <button>
             User#123
           </button>
