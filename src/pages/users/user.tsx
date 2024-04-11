960d66a3fd8977e5a5d23d51275d1d869db10745
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-04-11 23:07:39 +0200

    now using the route parameters with the correct typescript type

--- src/pages/users/user.tsx
+++ src/pages/users/user.tsx
@@ -1,11 +1,13 @@
 import { createMemo } from "solid-js";
 import { webHistorySearchParameters } from "../../web-history.tsx";
+import { WebHistoryRouteElementProps } from "../../library/history.tsx";
+import { Route } from "../../route.ts";
 
-export default function UserPage() {
+export default function UserPage({ user }: WebHistoryRouteElementProps<Route.User>) {
   const searchParameters = webHistorySearchParameters();
   const theme = createMemo(() => searchParameters.get("theme"));
 
   return (
-    <h1>User page with theme {theme()}</h1>
+    <h1>User#{user} page with theme {theme()}</h1>
   );
 }
